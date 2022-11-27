c0f978df35c1fccec7568880ad04250431874699
YardenSamorai <Yardensamorai6@gmail.com>
2022-11-27 17:21:03 +0200

    Init app

---
+++ src/components/Player.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+const Player = () => {
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range"></input>
+        <p>End Time</p>
+      </div>
+      <div className="play-constrol"></div>
+      <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+      <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+      <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
+    </div>
+  );
+};
+
+export default Player;
